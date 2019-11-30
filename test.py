a84f0bfbed8443c42b5a51b71cfab5e6b91858d4
Mickael-Roger <33000645+Mickael-Roger@users.noreply.github.com>
2019-11-30 19:12:03 +0100

    Update test.py

--- test.py
+++ test.py
@@ -14,12 +14,29 @@ class Thread_Distance(threading.Thread):
         global dist
         dist.obstacle()
 
+class Thread_Avance(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        global dist
+        global robot
+        try:
+            while True:
+                robot.avance(1)
+                print("Middle ", dist.middle, " left ", dist.left, " right ", dist.right)
+                if dist.middle < 30:
+                    robot.recule(5)
+                    robot.droite(90)
+         except KeyboardInterrupt:
+             robot.stop()
+
 
 arduino = Thread_Distance()
+bot = Thread_Avance()
+
 arduino.start()
+bot.start()
 
+bot.join()
 arduino.join()
-
-for i in range(1,50):
-    robot.avance(1)
-    print("Middle ", dist.middle, " left ", dist.left, " right ", dist.right)
