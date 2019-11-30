8aba555f9e4a0941df8c1ffd2256b88bae113500
Mickael-Roger <33000645+Mickael-Roger@users.noreply.github.com>
2019-11-30 17:47:32 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
-from robotlib import Robot
+from robotlib import Robot, SerialArduino
+import time
 
 
 robot = Robot()
+dist = SerialArduino()
 
+#for i in range(1,30):
+#    robot.avance(1)
+#    dist.readline()
+
+dist.readline()
 robot.avance(30)
-
-
+for i in range(1,50):
+    dist.readline()
+    time.sleep(0.300)
