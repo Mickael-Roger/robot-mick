5f176c56b5f96b9bcbd30a01dbb88e5edbe150fe
Mickael-Roger <33000645+Mickael-Roger@users.noreply.github.com>
2019-11-30 19:22:36 +0100

    Update test.py

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import threading
 
 robot = Robot()
 dist = SerialArduino()
+stop = 0
 
 class Thread_Distance(threading.Thread):
     def __init__(self):
@@ -21,22 +22,23 @@ class Thread_Avance(threading.Thread):
     def run(self):
         global dist
         global robot
-        try:
-            while True:
-                robot.avance(1)
-                print("Middle ", dist.middle, " left ", dist.left, " right ", dist.right)
-                if dist.middle < 20 or dist.left < 20 or dist.right < 20:
-                    robot.recule(2)
-                    robot.droite(90)
-        except KeyboardInterrupt:
-             robot.stop()
+        while stop == 0:
+            robot.avance(1)
+            print("Middle ", dist.middle, " left ", dist.left, " right ", dist.right)
+            if dist.middle < 20 or dist.left < 20 or dist.right < 20:
+                robot.recule(2)
+                robot.droite(90)
 
 
-arduino = Thread_Distance()
-bot = Thread_Avance()
+try:
+    arduino = Thread_Distance()
+    bot = Thread_Avance()
 
-arduino.start()
-bot.start()
+    arduino.start()
+    bot.start()
 
-bot.join()
-arduino.join()
+    bot.join()
+    arduino.join()
+
+except KeyboardInterrupt:
+    stop=1
